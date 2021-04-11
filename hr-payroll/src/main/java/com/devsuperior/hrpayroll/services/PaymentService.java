9accce8499553d9676870a0dea4fd190b8a0b368
lucasbarrossantos <lucas-barros28@hotmail.com>
2021-04-11 17:14:40 -0300

    FeignClient

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -2,30 +2,19 @@ package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.services.feignclient.WorkerFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class PaymentService {
-
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, Object> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId);
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-        assert worker != null;
+        Worker worker = workerFeignClient.findById(workerId);
 
         return Payment.builder()
                 .name(worker.getName())
